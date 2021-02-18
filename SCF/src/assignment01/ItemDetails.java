a53e11ad3bf98c35c0849a5fea10d0d158bf7b88
Neha Patidar <neha.patidar@metacube.com>
2021-02-18 13:59:54 +0530

    assisgment1 of SCF

---
+++ SCF/src/assignment01/ItemDetails.java
@@ -0,0 +1,11 @@
+package assignment01;
+//ItemDetails class  operation  perform by shop keeper
+//quantity and price of item
+public class ItemDetails {
+	int item_quantity;
+	float price_of_item;
+	ItemDetails(int item_quantity,float price_of_item){
+		this.item_quantity=item_quantity;
+		this.price_of_item=price_of_item;
+	}
+}
